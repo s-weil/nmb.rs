2937ca2f05d232f239fd2116ab4b8ba67d9c5b3c
Steffen <steffen.weil.ch@gmail.com>
2023-04-29 08:19:25 +0200

    cleaning up

--- statistics/src/samples.rs
+++ statistics/src/samples.rs
@@ -1,13 +1,3 @@
-// TODO: use ExactSizeIterator
-
-/// This trait is similiar to `ExactSizeIterator` but we adjust it for out own needs.
-// pub trait Samples<T>: std::iter::Iterator<Item = T> {
-//     fn is_empty(&self) -> bool {
-//         self.len() == 0
-//     }
-//     fn len(&self) -> usize;
-// }
-
 /// Should be the same as AsRef<&[T]> essentially
 pub trait AsSlice<T> {
     fn as_slice(&self) -> &[T];
@@ -24,33 +14,3 @@ impl<T> AsSlice<T> for Vec<T> {
         self
     }
 }
-
-// TODO. rename to samples
-// pub trait ExactSizeIteratorExt<T>: AsRef<T>
-// where
-//     T: ExactSizeIterator,
-// {
-//     fn is_empty(&self) -> bool {
-//         self.as_ref().len() == 0
-//     }
-// }
-// impl<'a, T> Samples<'a, T> for Vec<T> {
-//     fn len(&self) -> usize {
-//         (self as &Vec<T>).len()
-//     }
-// }
-
-// impl<'a, T> Samples<'a, T> for &'a Vec<T>
-// where
-//     T: Copy,
-// {
-//     fn len(&self) -> usize {
-//         (self as &'a Vec<T>).len()
-//     }
-// }
-
-// impl<'a, T> Samples<'a, T> for &'a [T] {
-//     fn len(&self) -> usize {
-//         (self as &[T]).len()
-//     }
-// }
